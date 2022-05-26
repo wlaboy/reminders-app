58d81cf745e1a1e1c8a6c9a3b2843e164b1272d8
Wilson Laboy <wil.j.laboy@gmail.com>
2022-05-26 16:41:43 -0400

    added reminders components, models, and services

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import NewReminder from "./components/NewReminder";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/reminder";
+import reminderService from "./services/reminder";
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
---
+++ src/components/NewReminder.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+interface NewReminderProps {
+  onAddReminder: (title: string) => void;
+}
+
+function NewReminder({ onAddReminder }: NewReminderProps): JSX.Element {
+  const [title, setTitle] = useState("");
+
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title) return;
+    onAddReminder(title);
+    setTitle("");
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="title">Add Reminder</label>
+      <div className="input-group mb-4">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          id="title"
+          type="text"
+          className="form-control"
+        />
+        <button type="submit" className="btn btn-primary rounded-pill ms-4">
+          Add Reminder
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default NewReminder;
